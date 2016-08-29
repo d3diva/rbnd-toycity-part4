d2d65f9ad2c13e1de017da9bb24da6f67c4f943f
d3diva <d3diva@gmail.com>
2016-08-29 03:23:19 -0400

    updated where method

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -3,14 +3,16 @@ module Analyzable
 
   def count_by_brand(products)
     brand = {}
-    products.each do |product|
-        brand[product.brand] == nil ? brand[product.brand] = 1 : brand[product.brand] += 1
-    end
-    brand
+    #products.each do |product|
+    #    brand[product.brand] == nil ? brand[product.brand] = 1 : brand[product.brand] += 1
+    #end
+    brand = products.each { |product| brand[.product.brand].nil ? brand[.product.brand] = 1 : brand[.product.brand] +=1 }
   end
 
-  #def count_by_name(products)
-  #end
+  def count_by_name(products)
+    name = {}
+    products.each
+  end
 
   def average_price(products)
     (products.inject(0) {|sum, el| sum + el.price.to_f} / products.size).round(2)
